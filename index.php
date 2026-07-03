c3416542aba69efe3049584deee6226003ea022d
helmiest <helmiestiawan@gmail.com>
2026-07-03 15:40:12 +0700

    Menambahkan tabel

--- index.php
+++ index.php
@@ -1,3 +1,56 @@
-<?php
-    // code here
-?>
+<html>
+<head>
+    <title>Daftar Lamaran</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="lamaran.php">Lamaran</a></li>
+                <li><a href="tentang.php">Tentang</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Daftar Lamaran</h1>
+        <button>Tambah Lamaran</button>
+        <br>
+        <table border="1">
+            <tr>
+                <th><input type="checkbox"></th>
+                <th>Perusahaan</th>
+                <th>Posisi</th>
+                <th>Lokasi</th>
+                <th>Tipe</th>
+                <th>Status</th>
+                <th>Tanggal Lamar</th>
+                <th>Portal</th>
+                <th>Aksi</th>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td>John Doe</td>
+                <td>john.doe@example.com</td>
+                <td>Web Developer</td>
+                <td>Remote</td>
+                <td>Open</td>
+                <td>2023-01-01</td>
+                <td>LinkedIn</td>
+                <td><button>View</button> <button>Timeline</button> <button>Edit</button> <button>Hapus</button> <button>Link</button></td>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td>John Doe</td>
+                <td>john.doe@example.com</td>
+                <td>Web Developer</td>
+                <td>Remote</td>
+                <td>Open</td>
+                <td>2023-01-01</td>
+                <td>LinkedIn</td>
+                <td><button>View</button> <button>Timeline</button> <button>Edit</button> <button>Hapus</button> <button>Link</button></td>
+            </tr>
+        </table>
+    </main>
+</body>
+</html>
